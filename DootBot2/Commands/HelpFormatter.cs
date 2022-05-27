93c38a807b82e1a5c1f8cb9d3ed97b9250be650f
ProTDC <danieltdc123@gmail.com>
2022-05-27 15:26:58 +0200

    Did some stuff

--- DootBot2/Commands/HelpFormatter.cs
+++ DootBot2/Commands/HelpFormatter.cs
@@ -7,12 +7,18 @@ namespace DootBot2.Commands
 {
     class HelpFormatter : DefaultHelpFormatter
     {
-        public HelpFormatter(CommandContext ctx) : base(ctx) { }
+        public HelpFormatter(CommandContext ctx) : base(ctx) 
+        {
+        }
 
         public override CommandHelpMessage Build()
         {
-            EmbedBuilder.Color = DiscordColor.Teal;
+            EmbedBuilder.Color = DiscordColor.Blue;
+            EmbedBuilder.Url = "https://www.youtube.com/watch?v=WTWyosdkx44";
+            EmbedBuilder.ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXSXaT2sNW2XGDEnvz5vEkJ5vD27XYpxb_Y9l96gmFxUH7ZbgwFhQsMm6_jlgr4kFifuo&usqp=CAU";
+
             return base.Build();
         }
+
     }
 }
--- DootBot2/Commands/Management.cs
+++ DootBot2/Commands/Management.cs
@@ -1,8 +1,8 @@
-﻿using Discord;
-using DSharpPlus;
+﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
 using System.Threading.Tasks;
 
 namespace DootBot2.Commands
@@ -34,9 +34,10 @@ namespace DootBot2.Commands
         {
             var embed = new DiscordEmbedBuilder
             {
-                Title = member.DisplayName,
+                Title = member.DisplayName + " #" + member.Discriminator,
                 Url = "https://www.youtube.com/watch?v=xvFZjo5PgG0",
-                Description = "this person is dumbass lol",
+                Description = "Description here",
+                Color = DiscordColor.Blue,
 
                 Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail
                 {
@@ -44,35 +45,23 @@ namespace DootBot2.Commands
                 }
             };
 
-            embed.AddField("UserID", member.Id.ToString());
+            embed.AddField("UserID:", member.Id.ToString());
             embed.AddField("Status:", member.Presence.Status.ToString());
-
-            if (member.Presence.Activity.Equals(DSharpPlus.Entities.ActivityType.Playing))
-            {
-                embed.AddField("Playing: ", member.Presence.Activity.Name.ToString());
-            }
-
-            //else if (member.Presence.Activity.Equals(DSharpPlus.Entities.ActivityType.ListeningTo))
-            //{
-            //    embed.AddField("Listening to: ", member.Presence.Activity.RichPresence.Details);
-            //}
-            
-            else
-            {
-                embed.AddField("Activity", "not currently doing anything");
-            }
-
+            embed.AddField("Has Permissions:", member.Permissions.ToPermissionString());
+            embed.AddField("Test:", member.Guild.Roles.ToString());  //doesnt work
+            embed.AddField("Is a Bot?", member.IsBot.ToString());
             embed.AddField("Created:", member.CreationTimestamp.DateTime.ToLongDateString());
-            embed.AddField("Joined at:", member.Guild.JoinedAt.DateTime.ToLongDateString());
+            embed.AddField("Joined this server at:", member.Guild.JoinedAt.DateTime.ToLongDateString());
 
-            await ctx.Channel.SendMessageAsync(embed: embed);
+            await ctx.Channel.SendMessageAsync(embed);
             return;
         }
+           
 
         [Command("addchnl")]
         [Description("Adds a channel (must be a moderator or higher)")]
         [RequireRoles(RoleCheckMode.Any, "Moderator", "Owner")]
-        public async Task addchnl(CommandContext ctx, string message)
+        public async Task Addchnl(CommandContext ctx, string message)
         {
             if (message.Contains(message))
             {
@@ -90,7 +79,7 @@ namespace DootBot2.Commands
         //[Command("delchnl")]
         //[Description("Deletes a channel (must be a moderator or higher)")]
         //[RequireRoles(RoleCheckMode.Any, "Moderator", "Owner")]
-        //public async Task delchnl(CommandContext ctx, string message)
+        //public async Task Delchnl(CommandContext ctx, string message)
         //{
         //    if (message.Contains(message))
         //    {
@@ -103,6 +92,22 @@ namespace DootBot2.Commands
         //        return;
         //    }
         //}
+
+        [Command("Silence")]
+        [Description("Adds a channel (must be a moderator or higher)")]
+        [RequireRoles(RoleCheckMode.Any, "Moderator", "Owner")]
+        public async Task Silence(CommandContext ctx, DiscordMember member)
+        {
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":ok_hand:");
+            var message = await ctx.RespondAsync($"{member.Mention}, react with {emoji}.");
+
+            var result = await message.WaitForReactionAsync(member, emoji);
+
+            if (!result.TimedOut)
+            {
+                await ctx.RespondAsync("Thank you!");
+            }
+        }
 
     }
 }
