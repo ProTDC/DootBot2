cfe4898b63944ec45a80b5d607e6f353030ccceb
ProTDC <danieltdc123@gmail.com>
2022-09-22 09:48:56 +0200

    did some stuff

--- DootBot2/Commands/RedditStuff.cs
+++ DootBot2/Commands/RedditStuff.cs
@@ -76,6 +76,36 @@ namespace DootBot2.Commands
             await ctx.RespondAsync(json["images"][0]["source"]["url"].ToString()).ConfigureAwait(false);
         }
 
+        [Command("Dankmemes")]
+        [Description("Post a meme from Reddit")]
+        public async Task redditMeme(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            var reddit = new RedditClient(appId: API_keys.redditAppID, appSecret: API_keys.redditAppSecret, refreshToken: API_keys.redditRefreshToken);
+            var random = new Random();
+            var url = new List<string> { "" };
+
+            IDictionary<string, IList<Post>> Posts = new Dictionary<string, IList<Post>>();
+            foreach (Post post in reddit.Subreddit("memes").Posts.Top)
+            {
+                if (!Posts.ContainsKey(post.Subreddit))
+                {
+                    Posts.Add(post.Subreddit, new List<Post>());
+                }
+                Posts[post.Subreddit].Add(post);
+
+                url.Add(post.Listing.Preview + " ");
+            }
+
+            int index = random.Next(url.Count);
+            string urlString = url[index];
+            var json = JObject.Parse(urlString);
+            Console.WriteLine(json);
+
+            await ctx.RespondAsync(json["images"][0]["source"]["url"].ToString()).ConfigureAwait(false);
+        }
+
 
     }
 }
